b985cee14279ce63f6a23e3a1ac57f77aa1cef1e
Leshan Sanjeewa <143873948+leshaan99@users.noreply.github.com>
2025-10-30 14:51:28 +0530

    Update config.php

--- controllers/config.php
+++ controllers/config.php
@@ -3,7 +3,7 @@
 define('DB_HOST','localhost');
 
 define('DB_USER','root');
-define('DB_PASS', '');
+define('DB_PASS','root');
 define('DB_NAME','project01');
 
 // define('DB_USER','u710400283_edu');
